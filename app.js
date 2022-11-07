3280461011ea0eea04e3196004da080e302566a1
Robert Hu <robert.y.hu.97@gmail.com>
2022-11-07 10:32:06 -0800

    React practice

--- app.js
+++ app.js
@@ -1,1 +1,38 @@
 // TODO
+const { useState } = React;
+
+const Items = (props) => {
+  const [hoverOver, setHoverOver] = useState(false);
+
+  const style = {
+    fontWeight: hoverOver ? 'bold' : 'normal'
+  };
+
+  return (
+    <li style={style} onMouseOver={() => setHoverOver(!hoverOver)}>
+      {props.groceryItem}
+    </li>
+  )
+
+};
+
+const GroceryListItems = (props) => (
+  <ul>
+    {props.groceryItems.map((groceryItem) => (
+      <Items groceryItem={groceryItem}/>
+    ))}
+  </ul>
+);
+
+const GroceryList = () => (
+  <div>
+    <GroceryListItems groceryItems={['Apple', 'Orange']} />
+  </div>
+);
+
+
+
+
+
+
+ReactDOM.render(<GroceryList />, document.getElementById("app"));
